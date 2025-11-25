e6cd5f6f847a8f2f887e0411cc9e32f387a93d7b
SmartPesa CEO <smartpesa@gmail.com>
2025-11-26 02:49:59 +0300

    Wire real PayPal link for Pro upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ import { useAuth } from "./context/AuthContext";
 
 
 // Put your real PayPal (or other) payment link here:
-const PAYMENT_LINK = "https://your-paypal-or-payment-link-here";
+const PAYMENT_LINK = "https://www.paypal.com/ncp/payment/8TPHFR6ZHSKDQ"
+
 
 // Default subs used for new users / guests
 const DEFAULT_SUBS = [
